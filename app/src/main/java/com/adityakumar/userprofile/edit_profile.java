96f4bc66753a6b3e6f76070c7dc9d16e792006c0
adityaanjeet <37912642+adityaanjeet@users.noreply.github.com>
2018-12-08 22:12:44 +0530

    pic update data upadte

---
+++ app/src/main/java/com/adityakumar/userprofile/edit_profile.java
@@ -0,0 +1,18 @@
+package com.adityakumar.userprofile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class edit_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+
+
+}
